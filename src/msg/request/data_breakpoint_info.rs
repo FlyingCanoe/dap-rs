c7bb1973cc3444807aa833a4b734b05529968e3d
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-07 15:03:36 -0500

    switch data breakpoint info request to macro

--- src/msg/request/data_breakpoint_info.rs
+++ src/msg/request/data_breakpoint_info.rs
@@ -1,27 +1,18 @@
-use serde_json as json;
-
-use crate::utils::{get_optional_u64, get_str};
-
-#[derive(Clone, Debug)]
-pub struct DataBreakpointInfoRequest {
-    /// Reference to the Variable container if the data breakpoint is requested for
-    /// a child of the container.
-    variables_reference: Option<u64>,
-
-    /// The name of the Variable's child to obtain data breakpoint information for.
-    /// If variablesReference isn't provided, this can be an expression.
-    name: String,
-}
-
-impl DataBreakpointInfoRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<DataBreakpointInfoRequest> {
-        let variables_reference = get_optional_u64(&msg, "variablesReference")?;
-        let name = get_str(&msg, "name")?.to_owned();
-
-        let request = DataBreakpointInfoRequest {
-            variables_reference,
-            name,
-        };
-        Ok(request)
+request!(
+    DataBreakpointInfoRequest {
+        optional_args = false;
+        u64 {},
+        Option<u64> {
+            /// Reference to the Variable container if the data breakpoint is requested for
+            /// a child of the container.
+            variables_reference: "variablesReference",
+        },
+        Option<bool> {},
+        String {
+            /// The name of the Variable's child to obtain data breakpoint information for.
+            /// If variablesReference isn't provided, this can be an expression.
+            name: "name",
+        },
+        Option<json::Value> {},
     }
-}
+);
