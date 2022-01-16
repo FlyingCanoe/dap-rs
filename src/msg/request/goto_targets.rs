bd7285e0dcb2d22bcdc4855083eda129a94005a2
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-16 13:59:43 -0500

    add goto target request

--- src/msg/request/goto_targets.rs
+++ src/msg/request/goto_targets.rs
@@ -1,10 +1,25 @@
-use serde_json as json;
+use crate::msg::dap_type::Source;
 
-#[derive(Clone, Debug, Hash)]
-pub struct GotoTargetsRequest {}
-
-impl GotoTargetsRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<GotoTargetsRequest> {
-        todo!()
+request!(
+    GotoTargetsRequest {
+        optional_args = false;
+        u64 {
+            /// The line location for which the goto targets are determined.
+            line: "line",
+        },
+        Option<u64> {
+            /// An optional column location for which the goto targets are determined.
+            column: "column",
+        },
+        Option<bool> {},
+        String {},
+        Option<json::Value> {},
+        Custom {
+            {
+                type = Source;
+                closure = Source::parse;
+                source: "source";
+            },
+        },
     }
-}
+);
