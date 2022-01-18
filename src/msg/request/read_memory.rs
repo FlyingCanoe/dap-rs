d74918dd0b94befa809d7162fcc77bd956ba4d37
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-18 11:58:16 -0500

    add read memory request

--- src/msg/request/read_memory.rs
+++ src/msg/request/read_memory.rs
@@ -1,10 +1,23 @@
-use serde_json as json;
-
-#[derive(Clone, Debug, Hash)]
-pub struct ReadMemoryRequest {}
-
-impl ReadMemoryRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<ReadMemoryRequest> {
-        todo!()
+request!(
+    ReadMemoryRequest {
+        optional_args = false;
+        u64 {
+            /// Number of bytes to read at the specified location and offset.
+            count: "count",
+        },
+        Option<u64> {
+            /// Optional offset (in bytes) to be applied to the reference location before
+            /// reading data. Can be negative.
+            offset: "offset",
+        },
+        Option<bool> {},
+        String {
+            /// Memory reference to the base location from which data should be read.
+            memory_reference: "memoryReference",
+        },
+        Option<String> {},
+        Option<json::Value> {},
+        Custom {},
+        Option<Custom> {},
     }
-}
+);
