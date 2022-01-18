482ed3fa9a2886939c602849becb3c61e4746f84
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-18 11:55:29 -0500

    add pause request

--- src/msg/request/pause.rs
+++ src/msg/request/pause.rs
@@ -1,10 +1,16 @@
-use serde_json as json;
-
-#[derive(Clone, Debug, Hash)]
-pub struct PauseRequest {}
-
-impl PauseRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<PauseRequest> {
-        todo!()
+request!(
+    PauseRequest {
+        optional_args = false;
+        u64 {
+            /// Pause execution for this thread.
+            thread_id: "threadId",
+        },
+        Option<u64> {},
+        Option<bool> {},
+        String {},
+        Option<String> {},
+        Option<json::Value> {},
+        Custom {},
+        Option<Custom> {},
     }
-}
+);
