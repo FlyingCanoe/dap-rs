3ddd73b35ff48264a80be59b6d7d500cbb87c10c
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-18 12:02:56 -0500

    add reverse continue request

--- src/msg/request/reverse_continue.rs
+++ src/msg/request/reverse_continue.rs
@@ -1,10 +1,22 @@
-use serde_json as json;
-
-#[derive(Clone, Debug, Hash)]
-pub struct ReverseContinueRequest {}
-
-impl ReverseContinueRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<ReverseContinueRequest> {
-        todo!()
+request!(
+    ReverseContinueRequest {
+        optional_args = false;
+        u64 {
+            /// Specifies the active thread. If the debug adapter supports single thread
+            /// execution (see 'supportsSingleThreadExecutionRequests') and the optional
+            /// argument 'singleThread' is true, only the thread with this ID is resumed.
+            thread_id: "threadId",
+        },
+        Option<u64> {},
+        Option<bool> {
+            /// If this optional flag is true, backward execution is resumed only for the
+            /// thread with given 'threadId'.
+            single_thread: "singleThread",
+        },
+        String {},
+        Option<String> {},
+        Option<json::Value> {},
+        Custom {},
+        Option<Custom> {},
     }
-}
+);
