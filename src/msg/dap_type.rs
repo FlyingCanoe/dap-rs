9e37eff6d39ee09defbea84f863375bf04243b8e
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-18 11:53:47 -0500

    add next request

--- src/msg/dap_type.rs
+++ src/msg/dap_type.rs
@@ -7,6 +7,7 @@ mod evaluate_ctx;
 mod path_format;
 mod presentation_hint;
 mod source;
+mod stepping_granularity;
 mod value_format;
 
 pub use breakpoint_location::BreakpointLocation;
@@ -18,4 +19,5 @@ pub use evaluate_ctx::EvaluateCtx;
 pub use path_format::PathFormat;
 pub use presentation_hint::PresentationHint;
 pub use source::Source;
+pub use stepping_granularity::SteppingGranularity;
 pub use value_format::ValueFormat;
