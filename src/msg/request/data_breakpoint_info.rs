fe1312a4fc5cb00674da21b9725187e7932a5ef4
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 13:47:26 -0500

    add data breakpoint info request

--- src/msg/request/data_breakpoint_info.rs
+++ src/msg/request/data_breakpoint_info.rs
@@ -1,22 +1,13 @@
-request!(
+use crate::utils::{parse_optional_u64, parse_string};
+
+request2!(
     DataBreakpointInfoRequest {
-        optional_args = false;
-        u64 {},
-        Option<u64> {
-            /// Reference to the Variable container if the data breakpoint is requested for
-            /// a child of the container.
-            variables_reference: "variablesReference",
-        },
-        Option<Vec<u64>> {},
-        Option<bool> {},
-        String {
-            /// The name of the Variable's child to obtain data breakpoint information for.
-            /// If variablesReference isn't provided, this can be an expression.
-            name: "name",
-        },
-        Option<String> {},
-        Option<json::Value> {},
-        Custom {},
-        Option<Custom> {},
+        /// Reference to the Variable container if the data breakpoint is requested for
+        /// a child of the container.
+        variables_reference | "variablesReference": Option<u64> = parse_optional_u64,
+
+        /// The name of the Variable's child to obtain data breakpoint information for.
+        /// If variablesReference isn't provided, this can be an expression.
+        name | "name": String = parse_string,
     }
 );
