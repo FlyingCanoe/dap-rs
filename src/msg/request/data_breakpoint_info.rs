fd6f0306bf91f29232f26cebe01d6368e1c49672
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 13:47:26 -0500

    add data breakpoint info request

--- src/msg/request/data_breakpoint_info.rs
+++ src/msg/request/data_breakpoint_info.rs
@@ -1,18 +1,11 @@
-use serde_json as json;
+request!(
+    DataBreakpointInfoRequest | "dataBreakpointInfo" {
+        /// Reference to the Variable container if the data breakpoint is requested for
+        /// a child of the container.
+        variables_reference | "variablesReference": Option<u64>,
 
-#[derive(Clone, Debug)]
-pub struct DataBreakpointInfoRequest {
-    /// Reference to the Variable container if the data breakpoint is requested for
-    /// a child of the container.
-    variables_reference: Option<u64>,
-
-    /// The name of the Variable's child to obtain data breakpoint information for.
-    /// If variablesReference isn't provided, this can be an expression.
-    name: String,
-}
-
-impl DataBreakpointInfoRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<DataBreakpointInfoRequest> {
-        todo!()
+        /// The name of the Variable's child to obtain data breakpoint information for.
+        /// If variablesReference isn't provided, this can be an expression.
+        name | "name": String,
     }
-}
+);
