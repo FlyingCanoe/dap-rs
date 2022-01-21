01fc65ef5145c76ed395fa9455d7e3ac73e1fd4a
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 13:07:05 -0500

    add attach request

--- src/msg/request/attach.rs
+++ src/msg/request/attach.rs
@@ -1,19 +1,12 @@
-request!(
-AttachRequest {
-    optional_args = false;
-    u64 {},
-    Option<u64> {},
-    Option<Vec<u64>> {},
-    Option<bool> {},
-    String {},
-    Option<String> {},
-    Option<json::Value> {
+use serde_json as json;
+
+request2!(
+    AttachRequest {
         /// Optional data from the previous, restarted session.
         /// The data is sent as the 'restart' attribute of the 'terminated' event.
         /// The client should leave the data intact.
-        restart_info: "__restart",
-    },
-    Custom {},
-    Option<Custom> {},
-}
+        restart | "__restart": Option<json::Value> = |value: Option<&json::Value>| -> anyhow::Result<Option<json::Value>> {
+            Ok(value.cloned())
+        },
+    }
 );
