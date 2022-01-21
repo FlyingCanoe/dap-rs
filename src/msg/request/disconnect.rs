62d49f47e830c32c3082291cf6551f1ed71778f8
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 13:47:43 -0500

    add disconnect request

--- src/msg/request/disconnect.rs
+++ src/msg/request/disconnect.rs
@@ -1,27 +1,23 @@
-request!(
+use crate::utils::parse_optional_bool;
 
+request2!(
     DisconnectRequest {
-        optional_args = true;
-        u64 {},
-        Option<u64> {},
-        Option<bool> {
-            /// A value of true indicates that this 'disconnect' request is part of a
-            /// restart sequence.
-            restart: "restart",
-            /// Indicates whether the debuggee should be terminated when the debugger is
-            /// disconnected.
-            /// If unspecified, the debug adapter is free to do whatever it thinks is best.
-            /// The attribute is only honored by a debug adapter if the capability
-            /// 'supportTerminateDebuggee' is true.
-            terminate_debuggee: "terminateDebuggee",
-            /// Indicates whether the debuggee should stay suspended when the debugger is
-            /// disconnected.
-            /// If unspecified, the debuggee should resume execution.
-            /// The attribute is only honored by a debug adapter if the capability
-            /// 'supportSuspendDebuggee' is true.
-            suspend_debuggee: "suspendDebuggee",
-        },
-        String {},
-        Custom {},
+        /// A value of true indicates that this 'disconnect' request is part of a
+        /// restart sequence.
+        restart | "restart": Option<bool> = parse_optional_bool,
+
+        /// Indicates whether the debuggee should be terminated when the debugger is
+        /// disconnected.
+        /// If unspecified, the debug adapter is free to do whatever it thinks is best.
+        /// The attribute is only honored by a debug adapter if the capability
+        /// 'supportTerminateDebuggee' is true.
+        terminate_debuggee | "terminateDebuggee": Option<bool> = parse_optional_bool,
+
+        /// Indicates whether the debuggee should stay suspended when the debugger is
+        /// disconnected.
+        /// If unspecified, the debuggee should resume execution.
+        /// The attribute is only honored by a debug adapter if the capability
+        /// 'supportSuspendDebuggee' is true.
+        suspend_debuggee | "suspendDebuggee": Option<bool> = parse_optional_bool,
     }
 );
