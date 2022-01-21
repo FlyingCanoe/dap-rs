1e37f6afe0af9f5e5bb66eb599a221d817e7193a
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 14:28:25 -0500

    add launch request

--- src/msg/request/launch.rs
+++ src/msg/request/launch.rs
@@ -1,22 +1,16 @@
-request!(
+use serde_json as json;
+
+use crate::utils::parse_optional_bool;
+
+request2!(
     LaunchRequest {
-        optional_args = false;
-        u64 {},
-        Option<u64> {},
-        Option<Vec<u64>> {},
-        Option<bool> {
-            /// If noDebug is true the launch request should launch the program without
-            /// enabling debugging.
-            no_debug: "noDebug",
-        },
-        String {},
-        Option<String> {},
-        Option<json::Value> {
-            /// Optional data from the previous, restarted session.
-            /// The data is sent as the 'restart' attribute of the 'terminated' event.
-            /// The client should leave the data intact.
-            restart_info: "__restart",
-        },
-        Custom {},
+        /// If noDebug is true the launch request should launch the program without
+        /// enabling debugging.
+        no_debug | "noDebug": Option<bool> = parse_optional_bool,
+
+        /// Optional data from the previous, restarted session.
+        /// The data is sent as the 'restart' attribute of the 'terminated' event.
+        /// The client should leave the data intact.
+        restart | "__restart": Option<json::Value> = |value: Option<&json::Value>| -> anyhow::Result<Option<json::Value>>  {Ok(value.cloned())},
     }
 );
