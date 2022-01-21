b5c59080f608f765ec091a48ae899abee6e3906b
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 13:54:00 -0500

    add exception info request

--- src/msg/request/exception_info.rs
+++ src/msg/request/exception_info.rs
@@ -1,10 +1,6 @@
-use serde_json as json;
-
-#[derive(Clone, Debug, Hash)]
-pub struct ExceptionInfoRequest {}
-
-impl ExceptionInfoRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<ExceptionInfoRequest> {
-        todo!()
+request!(
+    ExceptionInfoRequest | "exceptionInfo" {
+        /// Thread for which exception information should be retrieved.
+        thread_id | "threadId": u64,
     }
-}
+);
