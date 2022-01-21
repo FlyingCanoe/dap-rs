557b295141b10755df4653c3836c7a379053eee6
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 14:44:00 -0500

    add reverse continue request

--- src/msg/request/reverse_continue.rs
+++ src/msg/request/reverse_continue.rs
@@ -1,22 +1,14 @@
-request!(
+use crate::utils::{parse_optional_bool, parse_u64};
+
+request2!(
     ReverseContinueRequest {
-        optional_args = false;
-        u64 {
-            /// Specifies the active thread. If the debug adapter supports single thread
-            /// execution (see 'supportsSingleThreadExecutionRequests') and the optional
-            /// argument 'singleThread' is true, only the thread with this ID is resumed.
-            thread_id: "threadId",
-        },
-        Option<u64> {},
-        Option<Vec<u64>> {},
-        Option<bool> {
-            /// If this optional flag is true, backward execution is resumed only for the
-            /// thread with given 'threadId'.
-            single_thread: "singleThread",
-        },
-        String {},
-        Option<String> {},
-        Option<json::Value> {},
-        Custom {},
+        /// Specifies the active thread. If the debug adapter supports single thread
+        /// execution (see 'supportsSingleThreadExecutionRequests') and the optional
+        /// argument 'singleThread' is true, only the thread with this ID is resumed.
+        thread_id | "threadId": u64 = parse_u64,
+
+        /// If this optional flag is true, backward execution is resumed only for the
+        /// thread with given 'threadId'.
+        single_thread | "singleThread": Option<bool> = parse_optional_bool,
     }
 );
