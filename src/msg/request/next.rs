3782bf6b37dc792c540a0026f052d5acd3bef768
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 14:36:16 -0500

    add next request

--- src/msg/request/next.rs
+++ src/msg/request/next.rs
@@ -1,10 +1,16 @@
-use serde_json as json;
+use crate::msg::dap_type::SteppingGranularity;
 
-#[derive(Clone, Debug, Hash)]
-pub struct NextRequest {}
+request!(
+    NextRequest | "next" {
+        /// Specifies the thread for which to resume execution for one step (of the
+        /// given granularity).
+        thread_id | "threadId": u64,
 
-impl NextRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<NextRequest> {
-        todo!()
+        /// If this optional flag is true, all other suspended threads are not resumed.
+        single_thread | "singleThread": Option<bool>,
+
+        /// Optional granularity to step. If no granularity is specified, a granularity
+        /// of 'statement' is assumed.
+        granularity | "granularity": Option<SteppingGranularity>,
     }
-}
+);
