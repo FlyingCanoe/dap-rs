43427c4e692c0b0ed06536bf76fb89da6f177f6c
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 13:47:35 -0500

    add diassamble request

--- src/msg/request/diassemble.rs
+++ src/msg/request/diassemble.rs
@@ -1,36 +1,28 @@
-request!(
+use crate::utils::{parse_optional_bool, parse_optional_u64, parse_string, parse_u64};
+
+request2!(
     DiassambleRequest {
-        optional_args = false;
-        u64 {
-            /// Number of instructions to disassemble starting at the specified location
-            /// and offset.
-            /// An adapter must return exactly this number of instructions - any
-            /// unavailable instructions should be replaced with an implementation-defined
-            /// 'invalid instruction' value.
-            instruction_count: "instructionCount",
-        },
-        Option<u64> {
-            /// Optional offset (in bytes) to be applied to the reference location before
-            /// disassembling. Can be negative.
-            offset: "offset",
-            /// Optional offset (in instructions) to be applied after the byte offset (if
-            /// any) before disassembling. Can be negative.
-            instruction_offset: "instructionOffset",
-        },
-        Option<Vec<u64>> {},
-        Option<bool> {
-            /// If true, the adapter should attempt to resolve memory addresses and other
-            /// values to symbolic names.
-            resolve_symbols: "resolveSymbols",
-        },
-        String {
-            /// Memory reference to the base location containing the instructions to
-            /// disassemble.
-            memory_reference: "memoryReference",
-        },
-        Option<String> {},
-        Option<json::Value> {},
-        Custom {},
-        Option<Custom> {},
+        /// Memory reference to the base location containing the instructions to
+        /// disassemble.
+        memory_reference | "memoryReference": String = parse_string,
+
+        /// Optional offset (in bytes) to be applied to the reference location before
+        /// disassembling. Can be negative.
+        offset | "offset": Option<u64> = parse_optional_u64,
+
+        /// Optional offset (in instructions) to be applied after the byte offset (if
+        /// any) before disassembling. Can be negative.
+        instruction_offset | "instructionOffset": Option<u64> = parse_optional_u64,
+
+        /// Number of instructions to disassemble starting at the specified location
+        /// and offset.
+        /// An adapter must return exactly this u64 of instructions - any
+        /// unavailable instructions should be replaced with an implementation-defined
+        /// 'invalid instruction' value.
+        instruction_count | "instructionCount": u64 = parse_u64,
+
+        /// If true, the adapter should attempt to resolve memory addresses and other
+        /// values to symbolic names.
+        resolve_symbols | "resolveSymbols": Option<bool> = parse_optional_bool,
     }
 );
