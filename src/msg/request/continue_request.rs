e8885e02f006b892ab10c8380e0efe399f6dc651
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 13:41:05 -0500

    add continue request

--- src/msg/request/continue_request.rs
+++ src/msg/request/continue_request.rs
@@ -1,19 +1,9 @@
-use serde_json as json;
-
-#[derive(Clone, Debug)]
-pub struct ContinueRequest {
-    /// Specifies the active thread. If the debug adapter supports single thread
-    /// execution (see 'supportsSingleThreadExecutionRequests') and the optional
-    /// argument 'singleThread' is true, only the thread with this ID is resumed.
-    thread_id: u64,
-
-    /// If this optional flag is true, execution is resumed only for the thread
-    /// with given 'threadId'.
-    single_thread: Option<bool>,
-}
-
-impl ContinueRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<ContinueRequest> {
-        todo!()
+request!(
+    /// The request resumes execution of all threads. If the debug adapter supports single thread execution (see capability 'supportsSingleThreadExecutionRequests') setting the 'singleThread' argument to true resumes only the specified thread. If not all threads were resumed, the 'allThreadsContinued' attribute of the response must be set to false.
+    ContinueRequest | "continue" {
+        /// Specifies the active thread. If the debug adapter supports single thread execution (see 'supportsSingleThreadExecutionRequests') and the optional argument 'singleThread' is true, only the thread with this ID is resumed.
+        thread_id | "threadId": u64,
+        /// If this optional flag is true, execution is resumed only for the thread with given 'threadId'.
+        single_thread | "singleThread": Option<bool>,
     }
-}
+);
