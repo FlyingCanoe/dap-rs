af099cde5f5645f7235da0c2a8fd43639c1904cb
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 13:54:56 -0500

    add goto request

--- src/msg/request/goto.rs
+++ src/msg/request/goto.rs
@@ -1,19 +1,11 @@
-request!(
+use crate::utils::parse_u64;
+
+request2!(
     GotoRequest {
-        optional_args = false;
-        u64 {
-            /// Set the goto target for this thread.
-            thread_id: "threadId",
-            /// The location where the debuggee will continue to run.
-            target_id: "targetId",
-        },
-        Option<u64> {},
-        Option<Vec<u64>> {},
-        Option<bool> {},
-        String {},
-        Option<String> {},
-        Option<json::Value> {},
-        Custom {},
-        Option<Custom> {},
+        /// Set the goto target for this thread.
+        thread_id | "threadId": u64 = parse_u64,
+
+        /// The location where the debuggee will continue to run.
+        target_id | "targetId": u64 = parse_u64,
     }
 );
