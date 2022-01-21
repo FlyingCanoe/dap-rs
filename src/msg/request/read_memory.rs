3a2a380c462048833557bc00d56b72e746b4d451
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-21 14:39:38 -0500

    add read memory request

--- src/msg/request/read_memory.rs
+++ src/msg/request/read_memory.rs
@@ -1,23 +1,15 @@
-request!(
+use crate::utils::{parse_optional_u64, parse_string, parse_u64};
+
+request2!(
     ReadMemoryRequest {
-        optional_args = false;
-        u64 {
-            /// Number of bytes to read at the specified location and offset.
-            count: "count",
-        },
-        Option<u64> {
-            /// Optional offset (in bytes) to be applied to the reference location before
-            /// reading data. Can be negative.
-            offset: "offset",
-        },
-        Option<Vec<u64>> {},
-        Option<bool> {},
-        String {
-            /// Memory reference to the base location from which data should be read.
-            memory_reference: "memoryReference",
-        },
-        Option<String> {},
-        Option<json::Value> {},
-        Custom {},
+        /// Memory reference to the base location from which data should be read.
+        memory_reference | "memoryReference": String = parse_string,
+
+        /// Optional offset (in bytes) to be applied to the reference location before
+        /// reading data. Can be negative.
+        offset | "offset": Option<u64> = parse_optional_u64,
+
+        /// Number of bytes to read at the specified location and offset.
+        count | "count": u64 = parse_u64,
     }
 );
