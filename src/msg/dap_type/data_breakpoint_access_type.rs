ff6df8fdff85bea813405ac76ad4eed72df5e7b6
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 15:57:53 -0500

    add data breakpoint access type

--- src/msg/dap_type/data_breakpoint_access_type.rs
+++ src/msg/dap_type/data_breakpoint_access_type.rs
@@ -1,30 +1,7 @@
-use anyhow::{bail, Error};
-use serde_json as json;
-
-#[derive(Clone, Debug)]
-pub enum DataBreakpointAccessType {
-    Read,
-    Write,
-    ReadWrite,
-}
-
-impl DataBreakpointAccessType {
-    pub(crate) fn parse(input: Option<&json::Value>) -> anyhow::Result<DataBreakpointAccessType> {
-        let input = input.ok_or(Error::msg("parsing error"))?;
-        let input = input.as_str().ok_or(Error::msg("parsing error"))?;
-        let access_type = match input {
-            "read" => DataBreakpointAccessType::Read,
-            "write" => DataBreakpointAccessType::Write,
-            "readWrite" => DataBreakpointAccessType::ReadWrite,
-
-            _ => bail!("parsing error"),
-        };
-        Ok(access_type)
+dap_type_enum!(
+    DataBreakpointAccessType {
+        Read | "read",
+        Write | "write",
+        ReadWrite | "readWrite",
     }
-
-    pub(crate) fn parse_option(
-        input: Option<&json::Value>,
-    ) -> anyhow::Result<Option<DataBreakpointAccessType>> {
-        todo!()
-    }
-}
+);
