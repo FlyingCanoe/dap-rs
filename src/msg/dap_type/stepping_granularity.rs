79a135959e380fcb751ae2e7509d32b9da3ebab1
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 16:07:15 -0500

    add stepping granularity

--- src/msg/dap_type/stepping_granularity.rs
+++ src/msg/dap_type/stepping_granularity.rs
@@ -1,37 +1,7 @@
-use anyhow::{bail, Error};
-use serde_json as json;
-
-#[derive(Clone, Debug)]
-pub enum SteppingGranularity {
-    Statement,
-    Line,
-    Instruction,
-}
-
-impl SteppingGranularity {
-    pub(crate) fn parse(input: Option<&json::Value>) -> anyhow::Result<SteppingGranularity> {
-        let input = input
-            .ok_or(Error::msg("parsing error"))?
-            .as_str()
-            .ok_or(Error::msg("parsing error"))?;
-        let output = match input {
-            "statement" => SteppingGranularity::Statement,
-            "line" => SteppingGranularity::Line,
-            "instruction" => SteppingGranularity::Instruction,
-
-            _ => bail!("parsing error"),
-        };
-        Ok(output)
+dap_type_enum!(
+    SteppingGranularity {
+        Statement | "statement",
+        Line | "line",
+        Instruction | "instruction",
     }
-
-    pub(crate) fn parse_option(
-        input: Option<&json::Value>,
-    ) -> anyhow::Result<Option<SteppingGranularity>> {
-        if input.is_some() {
-            let output = SteppingGranularity::parse(input)?;
-            Ok(Some(output))
-        } else {
-            Ok(None)
-        }
-    }
-}
+);
