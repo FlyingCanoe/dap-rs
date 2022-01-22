934120ee7adb2d02007ef1e62e07a6291cec11a9
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 16:04:55 -0500

    add presentation hint

--- src/msg/dap_type/presentation_hint.rs
+++ src/msg/dap_type/presentation_hint.rs
@@ -1,37 +1,7 @@
-use anyhow::{bail, Error};
-use serde_json as json;
-
-#[derive(Clone, Debug)]
-pub enum PresentationHint {
-    Normal,
-    Emphasize,
-    Deemphasize,
-}
-
-impl PresentationHint {
-    pub(crate) fn parse(input: Option<&json::Value>) -> anyhow::Result<PresentationHint> {
-        let input = input
-            .ok_or(Error::msg("parsing error"))?
-            .as_str()
-            .ok_or(Error::msg("parsing error"))?;
-        let output = match input {
-            "normal" => PresentationHint::Normal,
-            "emphasize" => PresentationHint::Emphasize,
-            "deemphasize" => PresentationHint::Deemphasize,
-
-            _ => bail!("parsing error"),
-        };
-        Ok(output)
+dap_type_enum!(
+    PresentationHint {
+        Normal | "normal",
+        Emphasize | "emphasize",
+        Deemphasize | "deemphasize",
     }
-
-    pub(crate) fn parse_option(
-        input: Option<&json::Value>,
-    ) -> anyhow::Result<Option<PresentationHint>> {
-        if input.is_some() {
-            let output = PresentationHint::parse(input)?;
-            Ok(Some(output))
-        } else {
-            Ok(None)
-        }
-    }
-}
+);
