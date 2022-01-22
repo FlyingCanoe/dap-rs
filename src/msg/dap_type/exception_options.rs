58fe15febd5638a7fa2a11346e49f92bb3800f85
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 15:11:24 -0500

    add exception options

--- src/msg/dap_type/exception_options.rs
+++ src/msg/dap_type/exception_options.rs
@@ -1,25 +1,14 @@
-use serde_json as json;
-
-use crate::utils::Parse;
-
 use super::{ExceptionBreakMode, ExceptionPathSegment};
 
-#[derive(Debug, Clone)]
-pub struct ExceptionOptions {
-    /// A path that selects a single or multiple exceptions in a tree. If 'path' is
-    /// missing, the whole tree is selected.
-    /// By convention the first segment of the path is a category that is used to
-    /// group exceptions in the UI.
-    path: Option<Vec<ExceptionPathSegment>>,
-    /// Condition when a thrown exception should result in a break.
-    break_mode: ExceptionBreakMode,
-}
+dap_type_struct!(
+    ExceptionOptions {
+        /// A path that selects a single or multiple exceptions in a tree. If 'path' is
+        /// missing, the whole tree is selected.
+        /// By convention the first segment of the path is a category that is used to
+        /// group exceptions in the UI.
+        path | "path": Option<Vec<ExceptionPathSegment>>,
 
-impl Parse for ExceptionOptions {
-    fn parse(input: Option<&json::Value>) -> anyhow::Result<Self>
-    where
-        Self: Sized,
-    {
-        todo!()
+        /// Condition when a thrown exception should result in a break.
+        break_mode | "breakMode": ExceptionBreakMode,
     }
-}
+);
