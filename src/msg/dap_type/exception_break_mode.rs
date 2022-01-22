5c9f5417f2e028b740b81a74020e190ef8dfbea6
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 15:59:38 -0500

    add exception break mode

--- src/msg/dap_type/exception_break_mode.rs
+++ src/msg/dap_type/exception_break_mode.rs
@@ -1,28 +1,8 @@
-use anyhow::{bail, Error};
-use serde_json as json;
-
-#[derive(Clone, Debug)]
-pub enum ExceptionBreakMode {
-    Never,
-    Always,
-    Unhandled,
-    UserUnhandled,
-}
-
-impl ExceptionBreakMode {
-    pub(crate) fn parse(input: Option<&json::Value>) -> anyhow::Result<ExceptionBreakMode> {
-        let input = input
-            .ok_or(Error::msg("parsing error"))?
-            .as_str()
-            .ok_or(Error::msg("parsing error"))?;
-        let output = match input {
-            "never" => ExceptionBreakMode::Never,
-            "always" => ExceptionBreakMode::Always,
-            "unhandled" => ExceptionBreakMode::Unhandled,
-            "userUnhandled" => ExceptionBreakMode::UserUnhandled,
-
-            _ => bail!("parsing error"),
-        };
-        Ok(output)
+dap_type_enum!(
+    ExceptionBreakMode {
+        Never | "never",
+        Always | "always",
+        Unhandled | "unhandled",
+        UserUnhandled | "userUnhandled",
     }
-}
+);
