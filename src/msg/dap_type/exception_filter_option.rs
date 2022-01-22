a2d9d16826f2cb06da20199718860a633a385fa0
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 15:07:51 -0500

    add exception filter option

--- src/msg/dap_type/exception_filter_option.rs
+++ src/msg/dap_type/exception_filter_option.rs
@@ -1,54 +1,14 @@
-use anyhow::Error;
-use fallible_iterator::{convert, FallibleIterator};
-use serde_json as json;
-
 use crate::utils::{parse_optional_string, parse_string};
 
-#[derive(Debug, Clone)]
-pub struct ExceptionFilterOptions {
-    /// ID of an exception filter returned by the 'exceptionBreakpointFilters'
-    /// capability.
-    filter_id: String,
-    /// An optional expression for conditional exceptions.
-    /// The exception will break into the debugger if the result of the condition
-    /// is true.
-    condition: Option<String>,
-}
+dap_type_struct!(
+    ExceptionFilterOptions {
+        /// ID of an exception filter returned by the 'exceptionBreakpointFilters'
+        /// capability.
+        filter_id | "filterId": String = parse_string,
 
-impl ExceptionFilterOptions {
-    pub(crate) fn parse(input: Option<&json::Value>) -> anyhow::Result<ExceptionFilterOptions> {
-        let input = input.ok_or(Error::msg("parsing error"))?;
-        let filter_id = parse_string(input.get("filterId"))?;
-        let condition = parse_optional_string(input.get("condition"))?;
-
-        let output = ExceptionFilterOptions {
-            filter_id,
-            condition,
-        };
-        Ok(output)
+        /// An optional expression for conditional exceptions.
+        /// The exception will break into the debugger if the result of the condition
+        /// is true.
+        condition | "condition": Option<String> = parse_optional_string,
     }
-
-    pub(crate) fn parse_vec(
-        input: Option<&json::Value>,
-    ) -> anyhow::Result<Vec<ExceptionFilterOptions>> {
-        let input = input.ok_or(Error::msg("parsing error"))?;
-        let iter = input
-            .as_array()
-            .ok_or(Error::msg("parsing error"))?
-            .iter()
-            .map(|value| ExceptionFilterOptions::parse(Some(value)));
-        let output: Vec<_> = convert(iter).collect()?;
-        Ok(output)
-    }
-
-    pub(crate) fn parse_optional_vec(
-        input: Option<&json::Value>,
-    ) -> anyhow::Result<Option<Vec<ExceptionFilterOptions>>> {
-        if input.is_some() {
-            let output = ExceptionFilterOptions::parse_vec(input)?;
-            Ok(Some(output))
-        } else {
-            Ok(None)
-        }
-    }
-}
+);
