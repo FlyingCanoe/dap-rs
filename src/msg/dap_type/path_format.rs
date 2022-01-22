ce5e08af523935d97ae68305544f6a8d761ca5f1
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 16:01:42 -0500

    add path format

--- src/msg/dap_type/path_format.rs
+++ src/msg/dap_type/path_format.rs
@@ -1,26 +1,6 @@
-use anyhow::bail;
-use anyhow::Error;
-use serde_json as json;
-
-#[derive(Debug, Clone, Copy)]
-pub enum PathFormat {
-    Path,
-    Uri,
-}
-
-impl PathFormat {
-    pub(crate) fn parse_optional(
-        input: Option<&json::Value>,
-    ) -> anyhow::Result<Option<PathFormat>> {
-        if let Some(input) = input {
-            let path_format = match input.as_str().ok_or(Error::msg("parsing error"))? {
-                "path" => PathFormat::Path,
-                "uri" => PathFormat::Uri,
-                _ => bail!("parsing error"),
-            };
-            Ok(Some(path_format))
-        } else {
-            Ok(None)
-        }
+dap_type_enum!(
+    PathFormat {
+        Path | "path",
+        Uri | "uri",
     }
-}
+);
