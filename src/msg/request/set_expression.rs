5872bd4377edd093f6361f04e7da90001bb2d7d4
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 12:25:45 -0500

    add set expression request

--- src/msg/request/set_expression.rs
+++ src/msg/request/set_expression.rs
@@ -1,10 +1,18 @@
-use serde_json as json;
+use crate::msg::dap_type::ValueFormat;
 
-#[derive(Clone, Debug, Hash)]
-pub struct SetExpressionRequest {}
+request2!(
+    SetExpressionRequest {
+        /// The l-value expression to assign to.
+        expression | "expression": String,
 
-impl SetExpressionRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<SetExpressionRequest> {
-        todo!()
+        /// The value expression to assign to the l-value expression.
+        value | "value": String,
+
+        /// Evaluate the expressions in the scope of this stack frame. If not
+        /// specified, the expressions are evaluated in the global scope.
+        frame_id | "frameId": Option<u64>,
+
+        /// Specifies how the resulting value should be formatted.
+        format | "format": Option<ValueFormat>,
     }
-}
+);
