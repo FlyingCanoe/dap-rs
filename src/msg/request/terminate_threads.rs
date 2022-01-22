0687c29d4eafe4b1c18c0d3be7335df9f7708a3c
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-22 13:54:27 -0500

    add terminate threads request

--- src/msg/request/terminate_threads.rs
+++ src/msg/request/terminate_threads.rs
@@ -1,10 +1,6 @@
-use serde_json as json;
-
-#[derive(Clone, Debug, Hash)]
-pub struct TerminateThreadsRequest {}
-
-impl TerminateThreadsRequest {
-    pub(crate) fn parse(msg: json::Value) -> anyhow::Result<TerminateThreadsRequest> {
-        todo!()
+request2!(
+    TerminateThreadsRequest {
+        /// Ids of threads to be terminated.
+        thread_ids | "threadIds": Option<Vec<u64>>,
     }
-}
+);
