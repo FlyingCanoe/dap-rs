c9a9fbf0b00893627a96c526aa6b57468441d445
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-23 13:05:20 -0500

    add event

--- src/msg/event.rs
+++ src/msg/event.rs
@@ -1,10 +1,56 @@
+use anyhow::bail;
 use serde_json as json;
 
-#[derive(Clone, Debug, Hash)]
+use crate::utils::parse_string;
+
+macro_rules! event {
+    (
+        $event_name:ident {
+            $(
+                $(#[$field_meta:meta])*
+                $field:ident | $field_wire_name:literal: $field_ty:ty = $field_parsing_fn:expr,
+            )*
+        }
+    ) => {
+        #[derive(Clone, Debug)]
+        pub struct $event_name {
+            $(
+                $(#[$field_meta])*
+                $field: $field_ty,
+            )*
+        }
+
+        impl $event_name {
+            pub(crate) fn parse(msg: serde_json::Value) -> anyhow::Result<$event_name> {
+                let args = msg.get("body").ok_or(anyhow::Error::msg("invalid event"))?;
+
+                $(
+                    let value = msg.get($field_wire_name);
+                    let $field = $field_parsing_fn(value)?;
+                )*
+
+                let event = $event_name {
+                    $(
+                        $field,
+                    )*
+
+                };
+                Ok(event)
+            }
+        }
+    };
+}
+
+#[derive(Clone, Debug)]
 pub enum Event {}
 
 impl Event {
     pub(crate) fn parse(msg: json::Value) -> anyhow::Result<Event> {
-        todo!()
+        let event_type = parse_string(msg.get("event"))?;
+
+        let event = match event_type {
+            _ => bail!("invalid event"),
+        };
+        Ok(event)
     }
 }
