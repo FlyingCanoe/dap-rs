8dfb71f2f5469a899befdf06a74187ed7897c438
FlyingCanoe <flyingcanoe@protonmail.com>
2022-01-26 16:54:23 -0500

    fix capabilities name

--- src/msg/dap_type/capabilities.rs
+++ src/msg/dap_type/capabilities.rs
@@ -1,7 +1,6 @@
-use super::{ColumnDescriptor, ExceptionBreakpointsFilter};
 use crate::utils::{parse_optional_bool, parse_optional_string_vec};
 
-use super::ChecksumAlgorithm;
+use super::{ChecksumAlgorithm, ColumnDescriptor, ExceptionBreakpointsFilter};
 
 dap_type_struct!(
     Capabilities {
@@ -9,137 +8,137 @@ dap_type_struct!(
         supports_configuration_done_request | "supportsConfigurationDoneRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports function breakpoints.
-        supportsFunctionBreakpoints | "supportsFunctionBreakpoints": Option<bool> = parse_optional_bool,
+        supports_function_breakpoints | "supportsFunctionBreakpoints": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports conditional breakpoints.
-        supportsConditionalBreakpoints | "supportsConditionalBreakpoints": Option<bool> = parse_optional_bool,
+        supports_conditional_breakpoints | "supportsConditionalBreakpoints": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports breakpoints that break execution after a
         /// specified u64 of hits.
-        supportsHitConditionalBreakpoints | "supportsHitConditionalBreakpoints": Option<bool> = parse_optional_bool,
+        supports_hit_conditional_breakpoints | "supportsHitConditionalBreakpoints": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports a (side effect free) evaluate request for data
         /// hovers.
-        supportsEvaluateForHovers | "supportsEvaluateForHovers": Option<bool> = parse_optional_bool,
+        supports_evaluate_for_hovers | "supportsEvaluateForHovers": Option<bool> = parse_optional_bool,
 
         /// Available exception filter options for the 'setExceptionBreakpoints'
         /// request.
-        exceptionBreakpointFilters | "exceptionBreakpointFilters": Option<Vec<ExceptionBreakpointsFilter>> = ExceptionBreakpointsFilter::parse_optional_vec,
+        exception_breakpoint_filters | "exceptionBreakpointFilters": Option<Vec<ExceptionBreakpointsFilter>> = ExceptionBreakpointsFilter::parse_optional_vec,
 
         /// The debug adapter supports stepping back via the 'stepBack' and
         /// 'reverseContinue' requests.
-        supportsStepBack | "supportsStepBack": Option<bool> = parse_optional_bool,
+        supports_step_back | "supportsStepBack": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports setting a variable to a value.
-        supportsSetVariable | "supportsSetVariable": Option<bool> = parse_optional_bool,
+        supports_set_variable | "supportsSetVariable": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports restarting a frame.
-        supportsRestartFrame | "supportsRestartFrame": Option<bool> = parse_optional_bool,
+        supports_restart_frame | "supportsRestartFrame": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'gotoTargets' request.
-        supportsGotoTargetsRequest | "supportsGotoTargetsRequest": Option<bool> = parse_optional_bool,
+        supports_goto_targets_request | "supportsGotoTargetsRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'stepInTargets' request.
-        supportsStepInTargetsRequest | "supportsStepInTargetsRequest": Option<bool> = parse_optional_bool,
+        supports_step_in_targets_request | "supportsStepInTargetsRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'completions' request.
-        supportsCompletionsRequest | "supportsCompletionsRequest": Option<bool> = parse_optional_bool,
+        supports_completions_request | "supportsCompletionsRequest": Option<bool> = parse_optional_bool,
 
         /// The set of characters that should trigger completion in a REPL. If not
         /// specified, the UI should assume the '.' character.
-        completionTriggerCharacters | "completionTriggerCharacters": Option<Vec<String>> = parse_optional_string_vec,
+        completion_trigger_characters | "completionTriggerCharacters": Option<Vec<String>> = parse_optional_string_vec,
 
         /// The debug adapter supports the 'modules' request.
-        supportsModulesRequest | "supportsModulesRequest": Option<bool> = parse_optional_bool,
+        supports_modules_request | "supportsModulesRequest": Option<bool> = parse_optional_bool,
 
         /// The set of additional module information exposed by the debug adapter.
-        additionalModuleColumns | "additionalModuleColumns": Option<Vec<ColumnDescriptor>> = ColumnDescriptor::parse_optional_vec,
+        additional_module_columns | "additionalModuleColumns": Option<Vec<ColumnDescriptor>> = ColumnDescriptor::parse_optional_vec,
 
         /// Checksum algorithms supported by the debug adapter.
-        supportedChecksumAlgorithms | "supportedChecksumAlgorithms": Option<Vec<ChecksumAlgorithm>> = ChecksumAlgorithm::parse_optional_vec,
+        supported_checksum_algorithms | "supportedChecksumAlgorithms": Option<Vec<ChecksumAlgorithm>> = ChecksumAlgorithm::parse_optional_vec,
 
         /// The debug adapter supports the 'restart' request. In this case a client
         /// should not implement 'restart' by terminating and relaunching the adapter
         /// but by calling the RestartRequest.
-        supportsRestartRequest | "supportsRestartRequest": Option<bool> = parse_optional_bool,
+        supports_restart_request | "supportsRestartRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports 'exceptionOptions' on the
         /// setExceptionBreakpoints request.
-        supportsExceptionOptions | "supportsExceptionOptions": Option<bool> = parse_optional_bool,
+        supports_exception_options | "supportsExceptionOptions": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports a 'format' attribute on the stackTraceRequest,
         /// variablesRequest, and evaluateRequest.
-        supportsValueFormattingOptions | "supportsValueFormattingOptions": Option<bool> = parse_optional_bool,
+        supports_value_formatting_options | "supportsValueFormattingOptions": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'exceptionInfo' request.
-        supportsExceptionInfoRequest | "supportsExceptionInfoRequest": Option<bool> = parse_optional_bool,
+        supports_exception_info_request | "supportsExceptionInfoRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'terminateDebuggee' attribute on the
         /// 'disconnect' request.
-        supportTerminateDebuggee | "supportTerminateDebuggee": Option<bool> = parse_optional_bool,
+        support_terminate_debuggee | "supportTerminateDebuggee": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'suspendDebuggee' attribute on the
         /// 'disconnect' request.
-        supportSuspendDebuggee | "supportSuspendDebuggee": Option<bool> = parse_optional_bool,
+        support_suspend_debuggee | "supportSuspendDebuggee": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the delayed loading of parts of the stack, which
         /// requires that both the 'startFrame' and 'levels' arguments and an optional
         /// 'totalFrames' result of the 'StackTrace' request are supported.
-        supportsDelayedStackTraceLoading | "supportsDelayedStackTraceLoading": Option<bool> = parse_optional_bool,
+        supports_delayed_stack_trace_loading | "supportsDelayedStackTraceLoading": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'loadedSources' request.
-        supportsLoadedSourcesRequest | "supportsLoadedSourcesRequest": Option<bool> = parse_optional_bool,
+        supports_loaded_sources_request | "supportsLoadedSourcesRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports logpoints by interpreting the 'logMessage'
         /// attribute of the SourceBreakpoint.
-        supportsLogPoints | "supportsLogPoints": Option<bool> = parse_optional_bool,
+        supports_log_points | "supportsLogPoints": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'terminateThreads' request.
-        supportsTerminateThreadsRequest | "supportsTerminateThreadsRequest": Option<bool> = parse_optional_bool,
+        supports_terminate_threads_request | "supportsTerminateThreadsRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'setExpression' request.
-        supportsSetExpression | "supportsSetExpression": Option<bool> = parse_optional_bool,
+        supports_set_expression | "supportsSetExpression": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'terminate' request.
-        supportsTerminateRequest | "supportsTerminateRequest": Option<bool> = parse_optional_bool,
+        supports_terminate_request | "supportsTerminateRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports data breakpoints.
-        supportsDataBreakpoints | "supportsDataBreakpoints": Option<bool> = parse_optional_bool,
+        supports_data_breakpoints | "supportsDataBreakpoints": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'readMemory' request.
-        supportsReadMemoryRequest | "supportsReadMemoryRequest": Option<bool> = parse_optional_bool,
+        supports_read_memory_request | "supportsReadMemoryRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'writeMemory' request.
-        supportsWriteMemoryRequest | "supportsWriteMemoryRequest": Option<bool> = parse_optional_bool,
+        supports_write_memory_request | "supportsWriteMemoryRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'disassemble' request.
-        supportsDisassembleRequest | "supportsDisassembleRequest": Option<bool> = parse_optional_bool,
+        supports_disassemble_request | "supportsDisassembleRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'cancel' request.
-        supportsCancelRequest | "supportsCancelRequest": Option<bool> = parse_optional_bool,
+        supports_cancel_request | "supportsCancelRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'breakpointLocations' request.
-        supportsBreakpointLocationsRequest | "supportsBreakpointLocationsRequest": Option<bool> = parse_optional_bool,
+        supports_breakpoint_locations_request | "supportsBreakpointLocationsRequest": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'clipboard' context value in the 'evaluate'
         /// request.
-        supportsClipboardContext | "supportsClipboardContext": Option<bool> = parse_optional_bool,
+        supports_clipboard_context | "supportsClipboardContext": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports stepping granularities (argument 'granularity')
         /// for the stepping requests.
-        supportsSteppingGranularity | "supportsSteppingGranularity": Option<bool> = parse_optional_bool,
+        supports_stepping_granularity | "supportsSteppingGranularity": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports adding breakpoints based on instruction
         /// references.
-        supportsInstructionBreakpoints | "supportsInstructionBreakpoints": Option<bool> = parse_optional_bool,
+        supports_instruction_breakpoints | "supportsInstructionBreakpoints": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports 'filterOptions' as an argument on the
         /// 'setExceptionBreakpoints' request.
-        supportsExceptionFilterOptions | "supportsExceptionFilterOptions": Option<bool> = parse_optional_bool,
+        supports_exception_filter_options | "supportsExceptionFilterOptions": Option<bool> = parse_optional_bool,
 
         /// The debug adapter supports the 'singleThread' property on the execution
         /// requests ('continue', 'next', 'stepIn', 'stepOut', 'reverseContinue',
         /// 'stepBack').
-        supportsSingleThreadExecutionRequests | "supportsSingleThreadExecutionRequests": Option<bool> = parse_optional_bool,
+        supports_single_thread_execution_requests | "supportsSingleThreadExecutionRequests": Option<bool> = parse_optional_bool,
     }
 );
