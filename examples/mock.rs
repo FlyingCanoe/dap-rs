d012c30dafda68809f392f49f580c3d2c283a7bc
FlyingCanoe <flyingcanoe@protonmail.com>
2022-03-31 21:32:04 -0400

    refactor mock

--- examples/mock.rs
+++ examples/mock.rs
@@ -1,82 +1,97 @@
 use std::net;
 
-use dap::codec::DapCodec;
+use dap::codec::{DapCodec, Session};
 use dap::msg::dap_type::breakpoint::Breakpoint;
 use dap::msg::dap_type::capabilities::Capabilities;
 use dap::msg::dap_type::thread::Thread;
 use dap::msg::event::Event;
 use dap::msg::request::{
-    InitializeResponse, Request, RequestExt, SetBreakpointsResponse, ThreadsResponse,
+    InitializeRequest, InitializeResponse, Request, RequestExt, SetBreakpointsRequest,
+    SetBreakpointsResponse, ThreadsRequest, ThreadsResponse,
 };
 
 fn main() {
     let listener = net::TcpListener::bind("127.0.0.1:4710").unwrap();
     let mut codec = DapCodec::new(listener);
     loop {
-        let mut session = codec.accept().unwrap();
+        let session = codec.accept().unwrap();
+        run_session(session)
+    }
+}
 
-        let mut cap = Capabilities::default();
-        cap.supports_configuration_done_request = Some(true);
-
-        loop {
-            let request = session.recv_request().unwrap();
-            match request {
-                Request::Initialize(init_request) => init_request
-                    .respond(
-                        Ok(InitializeResponse {
-                            capabilities: Some(cap.clone()),
-                        }),
-                        &mut session,
-                    )
-                    .unwrap(),
-                Request::Launch(launch_request) => {
-                    launch_request.respond(Ok(()), &mut session).unwrap();
-                    session.send_event(Event::Initialized).unwrap();
-                }
-                Request::SetBreakpoints(set_breakpoint_request) => {
-                    let breakpoints = set_breakpoint_request
-                        .breakpoints
-                        .clone()
-                        .into_iter()
-                        .flat_map(Vec::into_iter)
-                        .map(|breakpoint| Breakpoint {
-                            source: Some(set_breakpoint_request.source.clone()),
-                            column: breakpoint.column,
-                            id: None,
-                            instruction_reference: None,
-                            line: Some(breakpoint.line),
-                            end_column: None,
-                            offset: None,
-                            verified: true,
-                            message: None,
-                            end_line: None,
-                        })
-                        .collect();
-
-                    set_breakpoint_request
-                        .respond(Ok(SetBreakpointsResponse { breakpoints }), &mut session)
-                        .unwrap();
-                }
-                Request::ConfigurationDone(config_done_request) => {
-                    config_done_request.respond(Ok(()), &mut session).unwrap()
-                }
-                Request::Threads(request) => request
-                    .respond(
-                        Ok(ThreadsResponse {
-                            threads: vec![Thread {
-                                id: 0,
-                                name: "main".to_string(),
-                            }],
-                        }),
-                        &mut session,
-                    )
-                    .unwrap(),
-                Request::Disconnect(request) => {
-                    request.respond(Ok(()), &mut session).unwrap();
-                    break;
-                }
-                _ => println!("{request:#?}"),
+fn run_session(mut session: Session) {
+    loop {
+        let request = session.recv_request().unwrap();
+        match request {
+            Request::Initialize(request) => handle_init_request(request, &mut session),
+            Request::Launch(launch_request) => {
+                launch_request.respond(Ok(()), &mut session).unwrap();
+                session.send_event(Event::Initialized).unwrap();
             }
+            Request::SetBreakpoints(request) => {
+                handle_set_breakpoints_request(request, &mut session)
+            }
+
+            Request::ConfigurationDone(request) => request.respond(Ok(()), &mut session).unwrap(),
+            Request::Threads(request) => handle_threads_request(request, &mut session),
+            Request::Disconnect(request) => {
+                request.respond(Ok(()), &mut session).unwrap();
+                break;
+            }
+            _ => println!("{request:#?}"),
         }
     }
+}
+
+fn handle_init_request(request: InitializeRequest, session: &mut Session) {
+    let mut cap = Capabilities::default();
+    cap.supports_configuration_done_request = Some(true);
+
+    request
+        .respond(
+            Ok(InitializeResponse {
+                capabilities: Some(cap.clone()),
+            }),
+            session,
+        )
+        .unwrap()
+}
+
+fn handle_set_breakpoints_request(request: SetBreakpointsRequest, session: &mut Session) {
+    let breakpoints = request
+        .breakpoints
+        .clone()
+        .into_iter()
+        .flat_map(Vec::into_iter)
+        .map(|breakpoint| Breakpoint {
+            source: Some(request.source.clone()),
+            column: breakpoint.column,
+            id: None,
+            instruction_reference: None,
+            line: Some(breakpoint.line),
+            end_column: None,
+            offset: None,
+            verified: true,
+            message: None,
+            end_line: None,
+        })
+        .collect();
+
+    request
+        .respond(Ok(SetBreakpointsResponse { breakpoints }), session)
+        .unwrap();
+}
+
+fn handle_threads_request(request: ThreadsRequest, session: &mut Session) {
+    request
+        .respond(
+            Ok(ThreadsResponse {
+                threads: vec![Thread {
+                    id: 0,
+                    name: "main".to_string(),
+                }],
+            }),
+            session,
+        )
+        .unwrap()
 }
