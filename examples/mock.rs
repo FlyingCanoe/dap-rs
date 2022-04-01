c1e496b4de9bbc926bb1df3ffe34ef9c679d8755
FlyingCanoe <flyingcanoe@protonmail.com>
2022-03-31 20:42:40 -0400

    add config done request to mock

--- examples/mock.rs
+++ examples/mock.rs
@@ -56,6 +56,9 @@ fn main() {
                     .respond(Ok(SetBreakpointsResponse { breakpoints }), &mut session)
                     .unwrap();
             }
+            Request::ConfigurationDone(config_done_request) => {
+                config_done_request.respond(Ok(()), &mut session).unwrap()
+            }
             _ => println!("{request:#?}"),
         }
     }
