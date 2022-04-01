2b3f733115d9c48c0a02f8feb835e5f3171a35e1
FlyingCanoe <flyingcanoe@protonmail.com>
2022-03-31 20:41:59 -0400

    add set breakpoints request to mock

--- examples/mock.rs
+++ examples/mock.rs
@@ -1,22 +1,22 @@
 use std::net;
 
 use dap::codec::DapCodec;
+use dap::msg::dap_type::breakpoint::Breakpoint;
 use dap::msg::dap_type::capabilities::Capabilities;
 use dap::msg::event::initialized::InitializedEvent;
 use dap::msg::event::Event;
-use dap::msg::request::{InitializeResponse, Request, RequestExt};
+use dap::msg::request::{InitializeResponse, Request, RequestExt, SetBreakpointsResponse};
 
 fn main() {
-    let listener = net::TcpListener::bind("127.0.0.1:4711").unwrap();
+    let listener = net::TcpListener::bind("127.0.0.1:4710").unwrap();
     let mut codec = DapCodec::new(listener);
     let mut session = codec.accept().unwrap();
 
     let mut cap = Capabilities::default();
-    cap.supports_function_breakpoints = Some(true);
+    cap.supports_configuration_done_request = Some(true);
 
     loop {
         let request = session.recv_request().unwrap();
-        println!("{request:#?}");
         match request {
             Request::Initialize(init_request) => init_request
                 .respond(
@@ -30,6 +30,30 @@ fn main() {
                 launch_request.respond(Ok(()), &mut session).unwrap();
                 session
                     .send_event(Event::Initialized(InitializedEvent {}))
+                    .unwrap();
+            }
+            Request::SetBreakpoints(set_breakpoint_request) => {
+                let breakpoints = set_breakpoint_request
+                    .breakpoints
+                    .clone()
+                    .into_iter()
+                    .flat_map(Vec::into_iter)
+                    .map(|breakpoint| Breakpoint {
+                        source: Some(set_breakpoint_request.source.clone()),
+                        column: breakpoint.column,
+                        id: None,
+                        instruction_reference: None,
+                        line: Some(breakpoint.line),
+                        end_column: None,
+                        offset: None,
+                        verified: true,
+                        message: None,
+                        end_line: None,
+                    })
+                    .collect();
+
+                set_breakpoint_request
+                    .respond(Ok(SetBreakpointsResponse { breakpoints }), &mut session)
                     .unwrap();
             }
             _ => println!("{request:#?}"),
