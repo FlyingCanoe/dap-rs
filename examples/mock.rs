5504fa667962476daca55d55b6f7fc584c644968
FlyingCanoe <flyingcanoe@protonmail.com>
2022-03-31 21:04:55 -0400

    add disconnect request to mock

--- examples/mock.rs
+++ examples/mock.rs
@@ -13,67 +13,73 @@ use dap::msg::request::{
 fn main() {
     let listner = net::TcpListener::bind("127.0.0.1:4710").unwrap();
     let mut codec = DapCodec::new(listner);
-    let mut session = codec.accept().unwrap();
-
-    let mut cap = Capabilities::default();
-    cap.supports_configuration_done_request = Some(true);
-
     loop {
-        let request = session.recv_request().unwrap();
-        match request {
-            Request::Initialize(init_request) => init_request
-                .respond(
-                    Ok(InitializeResponse {
-                        capabilities: Some(cap.clone()),
-                    }),
-                    &mut session,
-                )
-                .unwrap(),
-            Request::Launch(launch_request) => {
-                launch_request.respond(Ok(()), &mut session).unwrap();
-                session
-                    .send_event(Event::Initialized(InitializedEvent {}))
-                    .unwrap();
-            }
-            Request::SetBreakpoints(set_breakpoint_request) => {
-                let breakpoints = set_breakpoint_request
-                    .breakpoints
-                    .clone()
-                    .into_iter()
-                    .flat_map(Vec::into_iter)
-                    .map(|breakpoint| Breakpoint {
-                        source: Some(set_breakpoint_request.source.clone()),
-                        column: breakpoint.column,
-                        id: None,
-                        instruction_reference: None,
-                        line: Some(breakpoint.line),
-                        end_column: None,
-                        offset: None,
-                        verified: true,
-                        message: None,
-                        end_line: None,
-                    })
-                    .collect();
+        let mut session = codec.accept().unwrap();
 
-                set_breakpoint_request
-                    .respond(Ok(SetBreakpointsResponse { breakpoints }), &mut session)
-                    .unwrap();
+        let mut cap = Capabilities::default();
+        cap.supports_configuration_done_request = Some(true);
+
+        loop {
+            let request = session.recv_request().unwrap();
+            match request {
+                Request::Initialize(init_request) => init_request
+                    .respond(
+                        Ok(InitializeResponse {
+                            capabilities: Some(cap.clone()),
+                        }),
+                        &mut session,
+                    )
+                    .unwrap(),
+                Request::Launch(launch_request) => {
+                    launch_request.respond(Ok(()), &mut session).unwrap();
+                    session
+                        .send_event(Event::Initialized(InitializedEvent {}))
+                        .unwrap();
+                }
+                Request::SetBreakpoints(set_breakpoint_request) => {
+                    let breakpoints = set_breakpoint_request
+                        .breakpoints
+                        .clone()
+                        .into_iter()
+                        .flat_map(Vec::into_iter)
+                        .map(|breakpoint| Breakpoint {
+                            source: Some(set_breakpoint_request.source.clone()),
+                            column: breakpoint.column,
+                            id: None,
+                            instruction_reference: None,
+                            line: Some(breakpoint.line),
+                            end_column: None,
+                            offset: None,
+                            verified: true,
+                            message: None,
+                            end_line: None,
+                        })
+                        .collect();
+
+                    set_breakpoint_request
+                        .respond(Ok(SetBreakpointsResponse { breakpoints }), &mut session)
+                        .unwrap();
+                }
+                Request::ConfigurationDone(config_done_request) => {
+                    config_done_request.respond(Ok(()), &mut session).unwrap()
+                }
+                Request::Threads(request) => request
+                    .respond(
+                        Ok(ThreadsResponse {
+                            threads: vec![Thread {
+                                id: 0,
+                                name: "main".to_string(),
+                            }],
+                        }),
+                        &mut session,
+                    )
+                    .unwrap(),
+                Request::Disconnect(request) => {
+                    request.respond(Ok(()), &mut session).unwrap();
+                    break;
+                }
+                _ => println!("{request:#?}"),
             }
-            Request::ConfigurationDone(config_done_request) => {
-                config_done_request.respond(Ok(()), &mut session).unwrap()
-            }
-            Request::Threads(request) => request
-                .respond(
-                    Ok(ThreadsResponse {
-                        threads: vec![Thread {
-                            id: 0,
-                            name: "main".to_string(),
-                        }],
-                    }),
-                    &mut session,
-                )
-                .unwrap(),
-            _ => println!("{request:#?}"),
         }
     }
 }
