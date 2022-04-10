9a51edb94b43b3294813e3809ee6b017495ce8e7
FlyingCanoe <flyingcanoe@protonmail.com>
2022-04-09 21:17:11 -0400

    add macro

--- src/msg.rs
+++ src/msg.rs
@@ -1,2 +1,145 @@
+macro_rules! dap_type_struct {
+    (
+        $(#[$type_meta:meta])*
+        $type_name:ident {
+            $(
+                $(#[$field_meta:meta])*
+                $($field:ident).+ | $field_wire_name:literal: $field_ty:ty,
+            )*
+        }
+    ) => {
+        #[derive(Debug, Clone)]
+        $(#[$type_meta])*
+        pub struct $type_name {
+            $(
+                $(#[$field_meta])*
+                pub $($field)+: $field_ty,
+            )*
+        }
+
+        impl crate::utils::Parse for $type_name {
+            fn parse(input: Option<&serde_json::Value>) -> anyhow::Result<$type_name> {
+                let input = input.ok_or(anyhow::Error::msg("parsing error"))?;
+                $(
+                    let value = input.get($field_wire_name);
+                    let $($field)+ = <$field_ty>::parse(value)?;
+                )*
+
+                let output = $type_name {
+                    $(
+                        $($field)+,
+                    )*
+                };
+                Ok(output)
+            }
+        }
+
+        impl crate::utils::ToValue for $type_name {
+            fn to_value(self) -> Option<serde_json::Value> {
+                let mut map = serde_json::Map::new();
+                $(
+                    <$field_ty as crate::utils::ToValue>::to_value(self.$($field).+)
+                    .map(|value| {
+                            map.insert(
+                                $field_wire_name.to_string(),
+                                value
+                            )
+                        }
+                    );
+                )*
+                Some(map.into())
+            }
+        }
+    };
+}
+
+macro_rules! dap_type_enum {
+    (
+        $(#[$type_meta:meta])*
+        $type_name:ident {
+            Other,
+            $(
+                $(#[$field_meta:meta])*
+                $field:ident | $field_wire_name:literal,
+            )*
+        }
+    ) => {
+        #[derive(Clone, Debug)]
+        $(#[$type_meta])*
+        pub enum $type_name {
+            Other(String),
+            $(
+                $(#[$field_meta])*
+                $field,
+            )*
+        }
+
+        impl crate::utils::Parse for $type_name {
+            fn parse(input: Option<&serde_json::Value>) -> anyhow::Result<$type_name> {
+                let input = input
+                    .ok_or(anyhow::Error::msg("parsing error"))?
+                    .as_str()
+                    .ok_or(anyhow::Error::msg("parsing error"))?;
+                let output = match input {
+                    $($field_wire_name => $type_name::$field,)*
+                    _ => $type_name::Other(input.to_string()),
+                };
+                Ok(output)
+            }
+        }
+
+        impl crate::utils::ToValue for $type_name {
+            fn to_value(self) -> Option<serde_json::Value> {
+                let value = match self {
+                    $(Self::$field => serde_json::Value::String($field_wire_name.to_string()),)*
+                    Self::Other(value) =>serde_json::Value::String(value),
+                };
+                Some(value)
+            }
+        }
+    };
+    (
+        $(#[$type_meta:meta])*
+        $type_name:ident {
+            $(
+                $(#[$field_meta:meta])*
+                $field:ident | $field_wire_name:literal,
+            )*
+        }
+    ) => {
+        #[derive(Clone, Debug)]
+        $(#[$type_meta])*
+        pub enum $type_name {
+            $(
+                $(#[$field_meta])*
+                $field,
+            )*
+        }
+
+        impl crate::utils::Parse for $type_name {
+            fn parse(input: Option<&serde_json::Value>) -> anyhow::Result<$type_name> {
+                let input = input
+                    .ok_or(anyhow::Error::msg("parsing error"))?
+                    .as_str()
+                    .ok_or(anyhow::Error::msg("parsing error"))?;
+                let output = match input {
+                    $($field_wire_name => $type_name::$field,)*
+                    _ => anyhow::bail!("parsing error"),
+                };
+                Ok(output)
+            }
+        }
+
+        impl crate::utils::ToValue for $type_name {
+            fn to_value(self) -> Option<serde_json::Value> {
+                let value = match self {
+                    $(Self::$field => serde_json::Value::String($field_wire_name.to_string())),*
+                };
+                Some(value)
+            }
+        }
+    };
+}
+
 pub mod event;
 pub mod request;
--- src/msg/request.rs
+++ src/msg/request.rs
@@ -3,6 +3,198 @@ use serde_json as json;
 
 use crate::utils::Parse;
 
+macro_rules! request {
+    (
+        $(#[$request_meta:meta])*
+        $request_name:ident | $command:literal {
+            $(
+                $(#[$field_meta:meta])*
+                $($field:ident).+ | $field_wire_name:literal: $field_ty:ty,
+            )*
+        }
+    ) => {
+        #[derive(Debug)]
+        $(#[$request_meta])*
+        pub struct $request_name {
+            pub(crate) seq: u64,
+            $(
+                $(#[$field_meta])*
+                pub $($field)+: $field_ty,
+            )*
+        }
+
+        impl $request_name {
+            pub(crate) fn parse(msg: serde_json::Value) -> anyhow::Result<$request_name> {
+                let _args = msg.get("arguments");
+                let seq = msg.get("seq")
+                    .ok_or(anyhow::Error::msg("invalid msg"))?
+                    .as_u64()
+                    .ok_or(anyhow::Error::msg("invalid msg"))?;
+
+                $(
+                    let value = _args.ok_or(anyhow::Error::msg("invalid request"))?.get($field_wire_name);
+                    let $($field)+ = <$field_ty as crate::utils::Parse>::parse(value)?;
+                )*
+
+                let request = $request_name {
+                    seq,
+                    $($($field)+),*
+                };
+                Ok(request)
+            }
+        }
+    };
+    (
+        type Response = ();
+        $(#[$request_meta:meta])*
+        $request_name:ident | $command:literal {
+            $(
+                $(#[$field_meta:meta])*
+                $($field:ident).+ | $field_wire_name:literal: $field_ty:ty,
+            )*
+        }
+    ) => {
+        #[derive(Debug)]
+        $(#[$request_meta])*
+        pub struct $request_name {
+            pub(crate) seq: u64,
+            $(
+                $(#[$field_meta])*
+                pub $($field)+: $field_ty,
+            )*
+        }
+
+        impl $request_name {
+            pub(crate) fn parse(msg: serde_json::Value) -> anyhow::Result<$request_name> {
+                let _args = msg.get("arguments");
+                let seq = msg.get("seq")
+                    .ok_or(anyhow::Error::msg("invalid msg"))?
+                    .as_u64()
+                    .ok_or(anyhow::Error::msg("invalid msg"))?;
+
+                $(
+                    let value = _args.ok_or(anyhow::Error::msg("invalid request"))?.get($field_wire_name);
+                    let $($field)+ = <$field_ty as crate::utils::Parse>::parse(value)?;
+                )*
+
+                let request = $request_name {
+                    seq,
+                    $($($field)+),*
+                };
+                Ok(request)
+            }
+        }
+
+        impl crate::msg::request::RequestExt for $request_name {
+            type Response = ();
+
+            fn respond(
+                self,
+                response: Result<(), crate::msg::request::ErrorResponse>,
+                session: &mut crate::msg::request::Session,
+            ) -> Result<(), anyhow::Error> {
+                use crate::msg::request::{Response, AcknowledgementResponse};
+
+                let response = match response {
+                    Ok(_) => Response::from(AcknowledgementResponse::new($command.to_string())),
+                    Err(err) => Response::from(err),
+                };
+
+                session.send_response(response, self.seq)
+            }
+        }
+    };
+    (
+        type Response = $response:ty;
+        $(#[$request_meta:meta])*
+        $request_name:ident | $command:literal {
+            $(
+                $(#[$field_meta:meta])*
+                $($field:ident).+ | $field_wire_name:literal: $field_ty:ty,
+            )*
+        }
+    ) => {
+        #[derive(Debug)]
+        $(#[$request_meta])*
+        pub struct $request_name {
+            pub(crate) seq: u64,
+            $(
+                $(#[$field_meta])*
+                pub $($field)+: $field_ty,
+            )*
+        }
+
+        impl $request_name {
+            pub(crate) fn parse(msg: serde_json::Value) -> anyhow::Result<$request_name> {
+                let _args = msg.get("arguments");
+                let seq = msg.get("seq")
+                    .ok_or(anyhow::Error::msg("invalid msg"))?
+                    .as_u64()
+                    .ok_or(anyhow::Error::msg("invalid msg"))?;
+
+                $(
+                    let value = _args.ok_or(anyhow::Error::msg("invalid request"))?.get($field_wire_name);
+                    let $($field)+ = <$field_ty as crate::utils::Parse>::parse(value)?;
+                )*
+
+                let request = $request_name {
+                    seq,
+                    $($($field)+),*
+                };
+                Ok(request)
+            }
+        }
+    };
+
+}
+
+macro_rules! response {
+    (
+        $(#[$request_meta:meta])*
+        $response_name:ident | $command:literal {
+            $(
+                $(#[$field_meta:meta])*
+                $($field:ident).+ | $field_wire_name:literal: $field_ty:ty,
+            )*
+        }
+    ) => {
+        #[derive(Clone, Debug)]
+        $(#[$request_meta])*
+        pub struct $response_name {
+            $(
+                $(#[$field_meta])*
+                pub $($field).+: $field_ty,
+            )*
+        }
+
+        impl crate::utils::ToValue for $response_name {
+            fn to_value(self) -> Option<serde_json::Value> {
+                #[allow(unused_mut)]
+                let mut body = serde_json::Map::new();
+                let mut msg = serde_json::Map::new();
+
+                msg.insert("type".to_string(), "response".into());
+                msg.insert("success".to_string(), true.into());
+                msg.insert("command".to_string(), $command.into());
+
+                $(
+                    <$field_ty as crate::utils::ToValue>::to_value(self.$($field).+)
+                    .map(|value| {
+                            body.insert(
+                                $field_wire_name.to_string(),
+                                value
+                            )
+                        }
+                    );
+                )*
+
+                msg.insert("body".to_string(), body.into());
+                Some(msg.into())
+            }
+        }
+    };
+}
+
 mod attach;
 mod breakpoint_locations;
 mod completions;
