24488e2dddf6d4df4dd1bef5189b882015aa7dae
FlyingCanoe <flyingcanoe@protonmail.com>
2022-04-12 11:52:58 -0400

    add basic examples

---
+++ examples/basic.rs
@@ -0,0 +1,18 @@
+use std::net::TcpListener;
+
+use dap::connection::SocketConnection;
+
+fn main() {
+    // this example does not currently have any argument parsing.
+    // as such, it can not be ask to listen on a specific port, and instead use a hardcoded value.
+    let listener = TcpListener::bind("127.0.0.1:4711").unwrap();
+
+    for connection in listener.incoming() {
+        let mut connection = SocketConnection::new(connection.unwrap());
+
+        loop {
+            let header = connection.read_header().unwrap();
+            println!("{header}");
+        }
+    }
+}
