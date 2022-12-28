0d8ee0d408c69ccc4031a5d3a2cbf091801ed3bf
FlyingCanoe <flyingcanoe@protonmail.com>
2022-12-28 15:57:55 -0500

    fix disconnect request

--- examples/mock.rs
+++ examples/mock.rs
@@ -19,77 +19,88 @@ fn main() {
     let listener = net::TcpListener::bind("127.0.0.1:4711").unwrap();
     let codec = DapCodec::new(listener);
 
-    let mut session = codec.accept().unwrap();
-
     let cap = Capabilities::default();
 
     let init_event = Event::Initialized(InitializedEvent {});
     let init_event = MsgType::Event(init_event);
 
     loop {
-        let msg = session.recv().unwrap();
-        println!(
-            "{}",
-            serde_json::to_string_pretty(&msg.clone().to_value().unwrap()).unwrap()
-        );
+        let mut session = codec.accept().unwrap();
 
-        let MsgType::Request(request) = msg.msg_type else {panic!()};
+        loop {
+            let msg = session.recv().unwrap();
+            println!(
+                "{}",
+                serde_json::to_string_pretty(&msg.clone().to_value().unwrap()).unwrap()
+            );
 
-        let response = match request {
-            Request::Launch(request) => ResponseType::Acknowledgement(
-                AcknowledgementResponse::new(request.command().to_string()),
-            ),
-            Request::SetExceptionBreakpoints(_) => {
-                let response = SetExceptionBreakpointsResponse { breakpoints: None };
-                ResponseType::SetExceptionBreakpoints(response)
-            }
-            Request::Threads(_) => {
-                let response = ThreadsResponse {
-                    threads: vec![Thread {
-                        id: 0,
-                        name: "main".to_string(),
-                    }],
-                };
-                ResponseType::Threads(response)
-            }
-            Request::SetBreakpoints(_) => {
-                let response = SetBreakpointsResponse {
-                    breakpoints: vec![],
-                };
-                ResponseType::SetBreakpoints(response)
-            }
-            Request::Pause(_) => {
-                let response = PauseResponse {};
+            let MsgType::Request(request) = msg.msg_type else {panic!()};
 
-                ResponseType::Pause(response)
-            }
-            Request::Disconnect(_) => {
-                let response = DisconnectResponse {};
-                ResponseType::Disconnect(response)
-            }
-            Request::Initialize(_) => {
-                let response = InitializeResponse {
-                    capabilities: Some(cap.clone()),
-                };
-                let response = ResponseType::Initialize(response);
-                let response = Response {
+            let response = match request {
+                Request::Launch(request) => ResponseType::Acknowledgement(
+                    AcknowledgementResponse::new(request.command().to_string()),
+                ),
+                Request::SetExceptionBreakpoints(_) => {
+                    let response = SetExceptionBreakpointsResponse { breakpoints: None };
+                    ResponseType::SetExceptionBreakpoints(response)
+                }
+                Request::Threads(_) => {
+                    let response = ThreadsResponse {
+                        threads: vec![Thread {
+                            id: 0,
+                            name: "main".to_string(),
+                        }],
+                    };
+                    ResponseType::Threads(response)
+                }
+                Request::SetBreakpoints(_) => {
+                    let response = SetBreakpointsResponse {
+                        breakpoints: vec![],
+                    };
+                    ResponseType::SetBreakpoints(response)
+                }
+                Request::Pause(_) => {
+                    let response = PauseResponse {};
+
+                    ResponseType::Pause(response)
+                }
+                Request::Disconnect(_) => {
+                    let response = DisconnectResponse {};
+                    let response = ResponseType::Disconnect(response);
+
+                    session
+                        .send(MsgType::Response(Response {
+                            request_seq: msg.seq,
+                            response_type: response,
+                        }))
+                        .unwrap();
+
+                    break;
+                }
+                Request::Initialize(_) => {
+                    let response = InitializeResponse {
+                        capabilities: Some(cap.clone()),
+                    };
+                    let response = ResponseType::Initialize(response);
+                    let response = Response {
+                        request_seq: msg.seq,
+                        response_type: response,
+                    };
+
+                    session.send(MsgType::Response(response)).unwrap();
+
+                    session.send(init_event.clone()).unwrap();
+                    continue;
+                }
+                _ => panic!(),
+            };
+
+            session
+                .send(MsgType::Response(Response {
                     request_seq: msg.seq,
                     response_type: response,
-                };
-
-                session.send(MsgType::Response(response)).unwrap();
-
-                session.send(init_event.clone()).unwrap();
-                continue;
-            }
-            _ => panic!(),
-        };
-
-        session
-            .send(MsgType::Response(Response {
-                request_seq: msg.seq,
-                response_type: response,
-            }))
-            .unwrap();
+                }))
+                .unwrap();
+        }
     }
 }
